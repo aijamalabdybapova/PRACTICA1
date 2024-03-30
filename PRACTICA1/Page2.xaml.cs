061920c6c399b6e187b24e825cc4cf7b67ce1a02
aijamalabdybapova <isip_a.e.abdybapova@mpt.ru>
2024-03-30 11:58:18 +0300

    Добавьте файлы проекта.

---
+++ PRACTICA1/Page2.xaml.cs
@@ -0,0 +1,39 @@
+﻿using PRACTICA1.PRACTICADataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PRACTICA1
+{
+    /// <summary>
+    /// Логика взаимодействия для Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        //DirectorsTableAdapter directors = new DirectorsTableAdapter();
+        //public Page2()
+        //{
+        //    InitializeComponent();
+        //    Directorsdg.ItemsSource = directors.GetData();
+        //}
+
+        private PRACTICAEntities contextD = new PRACTICAEntities();
+        public Page2()
+        {
+            InitializeComponent();
+            Directorsdg.ItemsSource = contextD.Directors.ToList();
+
+        }
+    }
+}
